f4d03c058e18b7494a237a0b45420b1bdc146a63
Massimo Santoli <massimo@gimasi.ch>
2017-05-01 11:58:11 +0200

    updated tuino_lora example

--- tuino_libs/gmx/gmx_lr/tuino_lora/README.md
+++ tuino_libs/gmx/gmx_lr/tuino_lora/README.md
@@ -1,3 +1,130 @@
 # TUINO LORA
 This is an example sketch that shows the use of the GMX-LR1 library to join, trasmit and receive data via LoRa.</bR>
 You can use it as a blueprint for your LoRa projects and remember to install or copy in the same folder the GMX-LR libraries.
+# CODE DESCRIPTION
+The GMX-LR Module come with a fixed DevEui that cannot be change(*). AppEUI and AppKey for OTA provisioning, come preprogrammed, but can be changed with the gmxLR library calls. The preprogrammed values are:<b/>
+* AppEUI = DevEUI
+* AppKey = DevEUI,DevEUI
+so for example
+if DevEUI = 78AF580301020304
+then AppEUI = 78AF580301020304 and AppKEY = 78AF58030102030478AF580301020304
+When you change the AppKey and AppEUI they will stored in the EEPROM of the GMX-LR module and will saved also after power cycle.
+
+Here a quick explanation of the code:<br/>
+
+Init Section
+
+'''
+// Declare the interrupt callback function for RX
+
+bool data_received=false;
+
+void loraRx(){
+  data_received = true;
+}
+...
+...
+...
+
+// GMX-LR init pass callback function
+  gmxLR_init(&loraRx);
+
+...
+...
+
+// We now Join the NetWork
+// while loop checking the Join State
+while((join_status = gmxLR_isNetworkJoined()) != LORA_NETWORK_JOINED) {
+
+  
+    if ( join_wait == 0 )
+    {
+     // If AppKey and/or AppEUI are specified set them
+     if (AppEui.length() > 0 )
+      gmxLR_setAppEui(AppEui);
+     if (AppKey.length() > 0 )
+      gmxLR_setAppKey(AppKey);
+
+      // Disable Duty Cycle  ONLY FOR DEBUG!
+      gmxLR_setDutyCycle("0");
+      gmxLR_setClass("C");
+
+	  ...
+	  ...	
+	  ...
+
+	  // Here we trigger the Join Request
+      gmxLR_Join();
+    }
+
+    
+    join_wait++;
+
+    // Not necessary - but we reset everything after a timeout
+    if (!( join_wait % 100 )) {
+      gmxLR_Reset();
+      join_wait = 0;
+    }
+
+    delay(5000);
+
+  };
+
+'''
+
+Main Loop<br/>
+
+TX Section
+
+'''
+
+	...
+	...
+	// check delta TX Timeout
+	delta_lora_tx = millis() - timer_millis_lora_tx;
+   
+   // Transmit Period 
+   if ( delta_lora_tx > timer_period_to_tx) {
+  
+    Serial.println("TX DATA");
+
+  	// Transmit Data - as HEX String
+    gmxLR_TXData("010203");
+
+    ...
+    ...
+  
+     timer_millis_lora_tx = millis();
+   }
+'''
+
+RX Section - RX section is interrupt driven, when a RX packet arrives the loraRx() callback function is called and the data_received flag is set. We simply will simply test the flag to then read the RX buffer from the GMX-LR module.
+
+'''
+if (data_received)
+   {
+   	  // retrive RX data from GMX-LR module. 
+   	  // rx_data is a HEX String
+   	  // port is also returned
+      gmxLR_RXData(rx_data,&port);
+ 
+      // convert HEX String to byte array for simpler packet decoding
+      gmxLR_StringToHex(rx_data, rx_buf, rx_buf_len );
+
+      ...
+      ...
+
+      // simple protocol to turn on and off on board LED
+
+      if ( rx_buf[0] == 0x01 )
+        digitalWrite(LED_BUILTIN,HIGH);
+      else
+        digitalWrite(LED_BUILTIN,LOW);
+        
+      
+      // clear flag
+      data_received = false;
+   }
+'''
+
+The GMX-LR module uses the onboard leds to show it's state and activity, read here for a complete description
--- tuino_libs/gmx/gmx_lr/tuino_lora/tuino_lora.ino
+++ tuino_libs/gmx/gmx_lr/tuino_lora/tuino_lora.ino
@@ -8,14 +8,15 @@
 
  * tuino_lora.ino
  *
- *  Created on: March 11, 2017
+ *  Created on: April 30, 2017
  *      Author: Massimo Santoli
- *      Brief: Tuino Lora example
- *      Version: 1.0
+ *      Brief: Example Sketch to use LoRa GMX-LR1 Module
+ *      Version: 1.1
  *
  *      License: it's free - do whatever you want! ( provided you leave the credits)
+ *
  */
-
+ 
 #include "gmx_lr.h"
 
 long int timer_period_to_tx = 20000;
@@ -24,6 +25,7 @@ int ledState = 0;
 
 bool data_received=false;
 
+// Declare the interrupt callback function for RX
 void loraRx(){
   data_received = true;
 }
@@ -34,9 +36,11 @@ void setup() {
   String DevEui;
   String AppEui;
   String AppKey;
+  String _AppEui;
+  String _AppKey;
   String loraClass;
  
-  String adr,dcs;
+  String adr,dcs,dxrate;
 
   byte join_status;
   int join_wait;
@@ -44,47 +48,57 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Starting");
 
+  pinMode(LED_BUILTIN,OUTPUT);
+
   // GMX-LR init pass callback function
   gmxLR_init(&loraRx);
 
-  // put here the correct numbers
-  gmxLR_setDevEui("00:00:00:00:00:00:00:00");
-  gmxLR_setAppEui("00:00:00:00:00:00:00:00");
-  gmxLR_setAppKey("01:02:03:04:05:06:07:08:01:02:03:04:05:06:07:08");
+  // Set AppEui and AppKey 
+  // Uncomment if you want to change from default settings
+  // AppEui = "00:00:00:00:00:00:00:00";
+  // AppKey = "6d:41:46:39:67:4e:30:56:46:4a:62:4c:67:30:58:33";
 
-  Serial.println("LoRaWAN Params:");
-  gmxLR_getDevEui(DevEui);
-  Serial.println("DevEui:"+DevEui);
-  gmxLR_getAppEui(AppEui);
-  Serial.println("AppEui:"+AppEui);
-  gmxLR_getAppKey(AppKey);
-  Serial.println("AppKey:"+AppKey);
-  gmxLR_getClass(loraClass);
-  Serial.println("Class:"+loraClass);
-  adr = String( gmxLR_getADR() );
-  Serial.println("ADR:"+adr);
-  dcs = String( gmxLR_getDutyCycle() );
-  Serial.println("DCS:"+dcs);
-  
-  
   Serial.println("Joining...");
   join_wait = 0; 
   while((join_status = gmxLR_isNetworkJoined()) != LORA_NETWORK_JOINED) {
 
-  Serial.print("Join:");
-  Serial.println(join_wait);
   
-    if ( join_wait == 0 ) {
-      // we set again parameters 
-      gmxLR_setDevEui("00:00:00:00:00:00:00:00");
-      gmxLR_setAppEui("00:00:00:00:00:00:00:00");
-      gmxLR_setAppKey("01:02:03:04:05:06:07:08:01:02:03:04:05:06:07:08");
+    if ( join_wait == 0 )
+    {
+     // If AppKey and/or AppEUI are specified set them
+     if (AppEui.length() > 0 )
+      gmxLR_setAppEui(AppEui);
+     if (AppKey.length() > 0 )
+      gmxLR_setAppKey(AppKey);
+    
+      // Disable Duty Cycle  ONLY FOR DEBUG!
+      gmxLR_setDutyCycle("0");
+      gmxLR_setClass("C");
 
+      Serial.println("LoRaWAN Params:");
+      gmxLR_getDevEui(DevEui);
+      Serial.println("DevEui:"+DevEui);
+      gmxLR_getAppEui(_AppEui);
+      Serial.println("AppEui:"+_AppEui);
+      gmxLR_getAppKey(_AppKey);
+      Serial.println("AppKey:"+_AppKey);
+      gmxLR_getClass(loraClass);
+      Serial.println("Class:"+loraClass);
+      adr = String( gmxLR_getADR() );
+      Serial.println("ADR:"+adr);
+      dcs = String( gmxLR_getDutyCycle() );
+      Serial.println("DCS:"+dcs);
+      gmxLR_getRX2DataRate(dxrate);
+      Serial.println("RX2 DataRate:"+dxrate);
+      
       gmxLR_Join();
     }
-
+    Serial.print("Join:");
+    Serial.println(join_wait);
+  
     join_wait++;
 
+    // Not really necessary - but we reset everything after a timeout
     if (!( join_wait % 100 )) {
       gmxLR_Reset();
       join_wait = 0;
@@ -94,7 +108,6 @@ void setup() {
 
   };
   
-
   Serial.println("Network Joined!");
  
 }
@@ -106,33 +119,53 @@ void loop() {
   String RSSI;
   String SNR;
   String rx_data;
+  char rx_buf[128];
+  int rx_buf_len;
   int port;
+  int _snr;
 
    delta_lora_tx = millis() - timer_millis_lora_tx;
    
    // Transmit Period 
+   // check delta TX Timeout
    if ( delta_lora_tx > timer_period_to_tx) {
+    Serial.println("TX DATA");
 
+    // Transmit Data - as HEX String
     gmxLR_TXData("010203");
     
+    // get RSSI and SNR of last received packet
     gmxLR_getRSSI(RSSI);
     Serial.println("RSSI: "+RSSI);
     gmxLR_getSNR(SNR);
-    Serial.println("SNR: "+SNR);
+    _snr = SNR.toInt();
+    Serial.print("SNR: ");
+    Serial.println(_snr);
+    
 
      timer_millis_lora_tx = millis();
-
-     ledState = ~ledState;
-     gmxLR_Led(ledState); 
    }
 
    if (data_received)
    {
+      // retrive RX data from GMX-LR module. 
+      // rx_data is a HEX String
+      // port is also returned
       gmxLR_RXData(rx_data,&port);
+
+      //convert HEX String to byte array for simpler packet decoding
+      gmxLR_StringToHex(rx_data, rx_buf, rx_buf_len );
+
       Serial.println("LORA RX DATA:"+rx_data);  
       Serial.print("LORA RX PORT:");
       Serial.println(port);
-      
+
+      if ( rx_buf[0] == 0x01 )
+        digitalWrite(LED_BUILTIN,HIGH);
+      else
+        digitalWrite(LED_BUILTIN,LOW);
+        
+      // clear flag
       data_received = false;
    }
   
